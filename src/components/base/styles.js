160e2d747ecb6ed0581499a5d6395d88b3c0b76e
Kyle Mellander <kyle@tavour.com>
2020-08-24 09:34:47 -0700

    Default to elevation 3 for android to allow some shadow

--- src/components/base/styles.js
+++ src/components/base/styles.js
@@ -12,7 +12,8 @@ export default StyleSheet.create({
     backgroundColor: colors.white,
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.1,
-    shadowRadius: 6
+    shadowRadius: 6,
+    elevation: 3, // Adds shadow for Android
   },
   borderLeft: {
     borderLeftWidth: 5,
